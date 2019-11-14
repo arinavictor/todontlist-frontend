25139d8ed0a990edd76a9c56e2ac201dc5621771
arinavictor <arinavictor@yahoo.com>
2019-11-14 10:45:50 -0700

    makes add todo form component stateful, iniates state values of title and description to an empty string within a newTodo object and binds user input change to state

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function AddTodoForm() {
-    return (
-        <form className='list-form'>
-            <input 
-                type='text'
-                placeholder='Work, capstone project, etc...'
-               
-            />
-            <button id='form-submit' >Create List</button>
-           </form>
-    )
+export default class AddTodoForm extends Component{
+
+    state = {
+        newTodo: {
+            title: "",
+            description: "",
+        }
+    }
+
+    handleChange = property => event => {
+        const newTodo = this.state.newTodo
+        newTodo[property] = event.target.value
+        this.setState({ newTodo })
+        console.log("state change", this.state.newTodo)
+    }
+
+    render() {
+        return (
+            <form className='list-form'>
+                <input 
+                    type='text'
+                    placeholder='Todo Title'
+                    value={this.state.newTodo.title}
+                    onChange={this.handleChange("title")}
+                />
+
+                <input 
+                    type='text'
+                    placeholder="Todo Description"
+                    value={this.state.newTodo.description}
+                    onChange={this.handleChange("description")}
+                />
+              
+                <button id='form-submit' >Create List</button>
+               </form>
+        )
+    }
+   
 }
